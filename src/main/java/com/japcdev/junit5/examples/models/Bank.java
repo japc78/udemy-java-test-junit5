f62b9686f612edc3cc0fd281288862caa8219e17
japc78 <japc.grafico@gmail.com>
2021-05-10 18:01:50 +0200

    Asserts relations between Bank & Account

---
+++ src/main/java/com/japcdev/junit5/examples/models/Bank.java
@@ -0,0 +1,41 @@
+package com.japcdev.junit5.examples.models;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private String name;
+    private List<Account> accounts;
+
+    public Bank() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void addAccount(Account account) {
+        boolean add = this.accounts.add(account);
+        account.setBank(this);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void transfer(Account originAccount, Account targetAccount, BigDecimal amount) {
+        originAccount.debit(amount);
+        targetAccount.credit(amount);
+    }
+}
